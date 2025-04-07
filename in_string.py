03d4ba962de0d2a04453ce76c513aa8c02df7119
JuanaReig <jmreigaguilar@mail.austral.edu.ar>
2025-04-07 15:13:51 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,11 @@
 def check_vowels():
+    nombre = input()
+    nombre =nombre.lower()
+    print(f'"Contiene a:" {"a" in nombre}')
+    print(f'"Contiene e:" {"e" in nombre}')
+    print(f'"Contiene i:" {"i" in nombre}')
+    print(f'"Contiene o:" {"o" in nombre}')
+    print(f'"Contiene u:" {"u" in nombre}')
     # Código a implementar utilizando input.
 
 
