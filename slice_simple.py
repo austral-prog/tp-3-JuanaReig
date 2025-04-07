0488ce9508917fd35bcd46f7a6c0b306ec649359
JuanaReig <jmreigaguilar@mail.austral.edu.ar>
2025-04-07 15:32:54 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -3,7 +3,7 @@ def slice_simple():
     txt = texto.lower()
     print(txt[0:3])
     print(txt[2:5])
-    print(txt[0:5] + txt[-3:])
+    print(txt[0:4] + txt[-3:])
  
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
